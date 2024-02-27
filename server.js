ea89fa51558f103c14c72fded1b4f2231b306df9
Hanna Nesterova <nesterova.hanna@gmail.com>
2024-02-27 15:32:50 +0100

    try another password

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const mongoose = require('mongoose');
 
 mongoose.set("strictQuery" , false);
-mongoose.connect('mongodb+srv://nesterovahanna:w2vA*58Wb4VD6*j@cluster0.aspg3l1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://nesterovahanna:19931201jsIO@cluster0.aspg3l1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 //mongodb+srv://nesterovahanna:<password>@cluster0.aspg3l1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0//
-//password - w2vA*58Wb4VD6*j => по правилам их я также убирала * (w2vA%58Wb4VD6%j) => а еще так пробовала (w2vA%2A58Wb4VD6%2Aj)
+//password - 19931201!jsIO=> по правилам их я также убирала * (19931201%jsIO)
 
 app.listen(8000, () => {
     console.log('8000')
